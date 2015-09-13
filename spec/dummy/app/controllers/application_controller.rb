7e6ef75e91a13c50fa646a882ae0e1c8959acc09
Non Madden <nonmadden@gmail.com>
2015-09-13 22:03:28 +0700

    save for test

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    # raise BackgroundRemover::Base.new.inspect
-    render html: "xxx"
+
   end
 end
