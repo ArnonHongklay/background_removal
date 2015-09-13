2a0c1ba812bf08dab1fd158fdce4c44ec9a02889
-_-! <nonmadden@gmail.com>
2015-09-13 08:45:46 +0700

    fix spec

--- lib/background_remover.rb
+++ lib/background_remover.rb
@@ -1,6 +1,9 @@
+require 'background_remover/core_ext'
+
 module BackgroundRemover
   class Base
     include Singleton
+
     def name
       "BackgroundRemover"
     end
--- spec/background_remover_spec.rb
+++ spec/background_remover_spec.rb
@@ -1,7 +1,11 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe BackgroundRemover do
   it "is true" do
     expect(true).to be_truthy
+  end
+
+  it "has the name BackgroundRemover" do
+    expect(BackgroundRemover::Base.instance.name).to eql("BackgroundRemover")
   end
 end
