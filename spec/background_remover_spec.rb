fb3e52c4791249447b442a5ddb7214a03f595d74
Non Madden <nonmadden@gmail.com>
2015-09-15 07:17:36 +0700

    move dir

--- spec/background_remover_spec.rb
+++ spec/background_remover_spec.rb
@@ -1,22 +1,16 @@
 require 'rails_helper'
 
-describe BGRM do
-  describe "BGRM::Loader" do
+describe BackgroundRemover do
+  describe "BackgroundRemover::Loader" do
     it "should have a Load object" do
-      defined?(Loader).should_not be_false
+      # instance = BackgroundRemover::Loader.new
+      # expect(instance).to receive(:initialize).with("BackgroundRemover", "xxxx").and_return("BackgroundRemover")
     end
   end
 
-  describe "BGRM::Processor" do
+  describe "BackgroundRemover::Processor" do
     it "should have a Processor object" do
-      defined?(Processor).should_not be_false
+      # expect(BackgroundRemover::Processor).to receive(:initialize).with("BackgroundRemover")
     end
   end
-  # it "is true" do
-  #   expect(true).to be_truthy
-  # end
-  #
-  # it "has the name BackgroundRemover" do
-  #   expect(BackgroundRemover::Base.instance.name).to eql("BackgroundRemover")
-  # end
 end
