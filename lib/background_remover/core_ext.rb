a35021e7cb0590b4cb8cb976a06a84376c6d04fb
nonmadden <nonmadden@gmail.com>
2015-09-20 09:36:00 +0700

    cutter

--- lib/background_remover/core_ext.rb
+++ lib/background_remover/core_ext.rb
@@ -1,15 +1,19 @@
 module BackgroundRemover
-  @path = nil
+  @path = "./"
+
+  @path_original = nil
+  @path_finalize = nil
 
   class << self
-    attr_accessor :path
+    attr_accessor :path, :path_original, :path_finalize
   end
 
-  def self.loader(link, path)
-    img = BackgroundRemover::Loader.new("link", "path")
+  def self.loader(link)
+    img = BackgroundRemover::Loader.new(link, @path_original)
+    self.processor(img)
   end
 
-  def self.processor(path)
-    ps = BackgroundRemover::Processor.new("path")
+  def self.processor(img)
+    ps = BackgroundRemover::Processor.new(img, @path_original, @path_finalize)
   end
 end
